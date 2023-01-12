d4aea30346e1960114295d1bee08277b8fed1fa2
Luke <zlukeheysz@gmail.com>
2023-01-12 15:51:50 +0000

    LHeys-save

--- Unit03/command_line/rand.py
+++ Unit03/command_line/rand.py
@@ -2,7 +2,7 @@
 import argparse
 from random import randint
 
-# define an argument parser object
+# Define an argument parser object
 parser = argparse.ArgumentParser()
 
 # Add positional arguments
@@ -11,21 +11,21 @@ parser.add_argument('count', action = 'store', type = int, help = 'Count of rand
 # Add optional arguments
 parser.add_argument('-r', '--range', metavar = ('lower', 'upper'), nargs = 2, type = int, default = [0, 10], help = 'Integer range [a, b] from which the random numbers will be chosen')
 
-parser.add_argument('-c', '--const', action = 'store_const', const = 10, default = 0, help = 'generate 10 additional random numbers (in addition to Count)')
+parser.add_argument('-c', '--const', action = 'store_const', const = 10, default = 0, help = 'Generate 10 additional random numbers (in addition to Count)')
 
-parser.add_argument('-m', '--multiply', action = 'count', help = 'multiply the number of random numbers by the number of times this flag appears')
+parser.add_argument('-m', '--multiply', action = 'count', help = 'Multiply the number of random numbers by the number of times this flag appears')
 
 parser.add_argument('-v', '--verbose', action = 'store_true', help = 'Verbose mode')
 
-# parse command line arguments
+# Parse command-line arguments
 args = parser.parse_args()
 
-# program
+# Program
 
-# if args.const is used, add 10 to the count entered by the user
+# If args.const is used, add 10 to the count entered by the user
 num_of_rands = (args.count + args.const)
 
-# when args.multiply is not used, its value is None
+# When args.multiply is not used, its value is None
 if (args.multiply != None):
     num_of_rands = num_of_rands * args.multiply
 
